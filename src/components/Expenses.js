476588f7bdfcda932f20ec7fdaed1e0156308cb5
Mohammed Wajid <wajid17023@gmail.com>
2023-02-07 16:25:42 +0530

    added map function

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,38 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import ExpenseItem from './ExpenseItem'
-import Card from './Card'
-
-
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 const Expenses = (props) => {
   return (
-    <Card >
+    <Card>
+      {props.item.map(
+        (expense) => (
         <ExpenseItem
-        date={props.item[0].date}
-        title={props.item[0].title}
-        amount={props.item[0].amount}/>
-      
-
-      <ExpenseItem
-        date={props.item[1].date}
-        title={props.item[1].title}
-        amount={props.item[1].amount}/>
-      
-
-      <ExpenseItem
-        date={props.item[2].date}
-        title={props.item[2].title}
-        amount={props.item[2].amount}/>
-        
-
-      <ExpenseItem
-        date={props.item[3].date}
-        title={props.item[3].title}
-        amount={props.item[3].amount}/>
-      
+          date={expense.date}
+          title={expense.title}
+          amount={expense.amount}
+        />
+      ))}
     </Card>
-  )
-}
+  );
+};
 
-export default Expenses
+export default Expenses;
