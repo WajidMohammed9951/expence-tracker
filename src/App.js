cf8a82274d9c2a7f4e7d5bcc4eaff20de02daf6a
Mohammed Wajid <wajid17023@gmail.com>
2023-02-09 12:37:11 +0530

    removing app.js data to add apis

--- src/App.js
+++ src/App.js
@@ -3,36 +3,13 @@ import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 
+let DUMMY_EXPENSE = []
+
 
 const  App = () => {
-let DUMMY_EXPENSE = [
-  {
-  id: 'e1',
-  title:'school fee',
-  amount:'150',
-  date:'new Date(2021, 5, 12)'
-},
-{
-  id: 'e2',
-  title:'book rent',
-  amount:'200',
-  date:'new Date(2021, 2, 22)'
-},
-{
-  id: 'e3',
-  title:'hostel fee',
-  amount:'400',
-  date:'new Date(2021, 4, 2)'
-},
-{
-  id: 'e4',
-  title:'college fee',
-  amount:'200',
-  date:'new Date(2022, 5, 5)'
-}
-]
  
 const [expenses,setExpenses] = useState(DUMMY_EXPENSE);
+
 
 const addExpenseHandler = (expense) => {
   const updatedExpense = [expense, ...expenses];
